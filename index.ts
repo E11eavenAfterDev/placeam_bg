c61be55c8a2a3b7c747da063f8f4afbda8bcc28b
ikpeku <ikpedaniel07@gmail.com>
2024-09-06 12:03:51 +0100

    add route

--- index.ts
+++ index.ts
@@ -50,7 +50,7 @@ app.use("/api/user", adminroute)
 /**
  * test route
  */
-app.get("/", (req, res, next) => {
+app.use("/", (req, res, next) => {
     res.status(200).send("API is running");
 });
 
