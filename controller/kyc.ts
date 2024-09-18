811cca3f2894c82eb3e1f0653ee864c5fdc6f51b
ikpeku <ikpedaniel07@gmail.com>
2024-09-18 09:37:13 +0100

    add file upload

--- controller/kyc.ts
+++ controller/kyc.ts
@@ -1,8 +1,9 @@
 import KycModel from "../model/kyc"
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import { errorHandler } from "../utils/errorHandler";
 import { IRequest } from "../types";
-
+import { getDataUri } from "../utils/features";
+import { v2 as cloudinary } from 'cloudinary';
 
 export const updateKyc = async (req: IRequest, res: Response, next: NextFunction) => {
     const {
@@ -10,18 +11,80 @@ export const updateKyc = async (req: IRequest, res: Response, next: NextFunction
         firstname,
         lastname,
         address,
-        document_back_view,
-        document_front_view,
         document_type,
         bank_name,
         account_number,
         account_name
     } = req.body
-  
+
+
+if(!req.files) {
+     return errorHandler(res, 500,"kyc information incomplete")
+}
+if(req.files?.length == 0) {
+     return errorHandler(res, 500,"kyc information incomplete")
+}
+
+ const items:{
+ [key: string]: any,
+ } = req.files;
+
 
     try {
 
         if(!req.payload) return errorHandler(res, 500,"user not login in" )
+
+
+            let document_back_view;
+            let document_front_view;
+
+
+            for (const image in items.document_front_view){
+
+                const file =  getDataUri(items.document_front_view[image])
+              const result = await cloudinary.uploader.upload(file.content!, {folder: "placeAmUserProfilePhotos", public_id: `${email}_front`, overwrite: true, filename_override: `${email}_front`});
+              
+              const url =  cloudinary.url(result.public_id, {
+                transformation: [
+                    {
+                        quality: "auto",
+                        fetch_format: "auto",
+                    },
+                    {
+                        width: 500,
+                        height: 500,
+                        crop: "fill",
+                        gravity: "auto"
+                    }
+                ]
+              })
+              
+              document_front_view = url
+            }
+        
+            for (const image in items.document_back_view){
+        
+                const file =  getDataUri(items.document_back_view[image])
+              const result = await cloudinary.uploader.upload(file.content!, {folder: "placeAmUserProfilePhotos", public_id:`${email}_back`, overwrite: true, filename_override: `${email}_back`});
+             
+              const url =  cloudinary.url(result.public_id, {
+                transformation: [
+                    {
+                        quality: "auto",
+                        fetch_format: "auto",
+                    },
+                    {
+                        width: 500,
+                        height: 500,
+                        crop: "fill",
+                        gravity: "auto"
+                    }
+                ]
+              })
+             
+              document_back_view = url
+            }
+        
 
         const response = await KycModel.findByIdAndUpdate(req.payload.userId, {
             id_proof: {
--- routes/user.ts
+++ routes/user.ts
@@ -2,14 +2,14 @@ import express from "express"
 import { donateItem, getdonateUserItem, updateUser, updateUserAvatar } from "../controller/user";
 import { onlyLoginUser } from "../utils/helper";
 import { getKyc, updateKyc } from "../controller/kyc";
-import { singleupload } from "../middleware/multer";
+import { multipleupload, singleupload } from "../middleware/multer";
 
 
 const userroute = express.Router()
 
 userroute.patch("/update", onlyLoginUser, updateUser)
 userroute.get("/kyc", onlyLoginUser, getKyc)
-userroute.patch("/kyc", onlyLoginUser, updateKyc)
+userroute.patch("/kyc", onlyLoginUser, multipleupload, updateKyc)
 userroute.patch("/avatar", onlyLoginUser, singleupload, updateUserAvatar)
 userroute.post("/donation", onlyLoginUser, donateItem)
 userroute.get("/donation", onlyLoginUser, getdonateUserItem)
